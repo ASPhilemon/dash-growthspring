967efc3683758bde6b9532a316b0cea679a497ec
ASPhilemon <philemonariko@gmail.com>
2025-02-26 22:06:45 +0300

    u

--- src/hooks/useSubmitForm.js
+++ src/hooks/useSubmitForm.js
@@ -8,33 +8,44 @@ export function useSubmitForm({url, method = "POST", beforeSubmit, onSuccess = (
     async function handleSubmit(e){
       e.preventDefault()
       beforeSubmit()
-      setIsLoading(true)
-      setError(null)
-      setData(null)
+      
       const form = e.target
       let formData = new FormData(form)
       formData = Object.fromEntries(formData.entries()) 
+
+      const isConfirmed = window.confirm(`You are sending ${formData.points} points to ${formData.beneficiary} `);
+      if (!isConfirmed) {
+        return
+      }
+
       console.log("form Data", formData )
 
-      try {
-        const res = await fetch(url, {
-          method: method,
-          body: JSON.stringify(formData),
-          credentials: "include"
-        })
-        const data = await res.json()
-        if (res.ok){
-          onSuccess(data, formData )
-          setData(data)
-        } else {
-          setData(null)
-          setError(new Error(data.message))
-        }
-      } catch(err){
-        setError(err)
+      async function submit(){
+        setIsLoading(true)
+        setError(null)
         setData(null)
+        try {
+          const res = await fetch(url, {
+            method: method,
+            body: JSON.stringify(formData),
+            credentials: "include"
+          })
+          const data = await res.json()
+          if (res.ok){
+            onSuccess(data, formData )
+            setData(data)
+          } else {
+            setData(null)
+            setError(new Error(data.message))
+          }
+        } catch(err){
+          setError(err)
+          setData(null)
+        }
+        setIsLoading(false)
       }
-      setIsLoading(false)
+
+      submit()
     }
 
   return {isLoading, data, error, setIsLoading, setData, setError, handleSubmit}
